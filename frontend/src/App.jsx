c52f3361dde17b57cacc7394c3dafa8ebd11bd8e
Shaik Mahammad jani <skskjani7@gmail.com>
2025-02-24 18:08:16 +0530

    back end 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" />
-    </Routes>
-  );
+  return <></>;
 };
 
 export default App;
